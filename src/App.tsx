2370f52093eb7416b139d0fe93917958e3de1c1f
Adam Nerderman <nerderma@oclc.org>
2023-05-01 13:22:42 -0400

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import { Food, foods } from "./types/food";
+import { foods } from "./types/food";
 
 export function App() {
-  function renderFood(food: Food) {
-    return <p>{food.name}</p>;
-  }
-
+  const appetizers = foods.filter((food) => food.tags.includes("Appetizer"));
   return (
     <>
       <h1>Menu</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <h2>Appetizers</h2>
-      {foods.map(renderFood)}
+      {appetizers.map((food) => (
+        <p key={food.id}>
+          {food.name} - ${food.price}
+        </p>
+      ))}
     </>
   );
 }
